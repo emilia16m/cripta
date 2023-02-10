ed2921b66515e4ae7cea4c329292c0fbfacf9e2a
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-10 18:22:28 +0300

    Create descr.jsx

---
+++ src/components/descr.jsx
@@ -0,0 +1,15 @@
+
+
+const Descr = () => {
+
+    return (
+        <div className="descr">
+            <div className="container">
+                <h2>Crypto Exchange</h2>
+                <p>Exchange fast and easy</p>
+            </div>
+        </div>
+    )
+}
+
+export default Descr;
